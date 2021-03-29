e63257c851dc402d4f6dde8e9a2c71394c3e6d07
TamikaHayes <tamika.hayes@gmail.com>
2021-03-29 13:31:48 -0700

    Set new defaults on page load
    --set focus to text input field "Name:"
    --hide text input field for "Other job role"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('Test');
+/**
+ * Treehouse Techdegree Project 3
+ * Interactive Form
+ * Tamika Hayes, March 28,2021
+ */
+
+/**
+ * This project is an interactive registration form that validates user input 
+ * for a fictional full stack web development conference.
+ */
+
+//prompt user input by setting focus to first text input field, "Name:"
+const nameInput = document.getElementById("name");
+//console.log(nameInput);
+nameInput.focus();
+
+//hide "other job role" text field
+const otherJobRoleInput = document.getElementById("other-job-role");
+//console.log(otherJobRoleInput);
+otherJobRoleInput.style.display = "none";
+
+//add event listener to job role drop-down menu; display text field if user selects "Other"
