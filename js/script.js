b41a3d4cda6847d7f6a6c3e006d04ffc1a1c7c92
TamikaHayes <tamika.hayes@gmail.com>
2021-03-29 14:48:47 -0700

    Added change event listener to <select> menu for job role
    --text input field for "Other job role" is displayed only when "Other" is selected from menu

--- js/script.js
+++ js/script.js
@@ -11,12 +11,21 @@
 
 //prompt user input by setting focus to first text input field, "Name:"
 const nameInput = document.getElementById("name");
-//console.log(nameInput);
 nameInput.focus();
 
 //hide "other job role" text field
 const otherJobRoleInput = document.getElementById("other-job-role");
-//console.log(otherJobRoleInput);
 otherJobRoleInput.style.display = "none";
 
 //add event listener to job role drop-down menu; display text field if user selects "Other"
+const userJobTitle = document.getElementById("title");
+
+userJobTitle.addEventListener('change', function() {
+    if (userJobTitle.value === "other") {
+        if (otherJobRoleInput.style.display = "none") {
+            otherJobRoleInput.style.display = "block";
+        } else {
+            otherJobRoleInput.style.display = "none";
+        }  
+    } 
+});
